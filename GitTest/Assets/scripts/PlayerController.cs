b79aeb2053b3446042b86f1499dda7d8c376537c
Slimsy5211 <jaguilar23@mail.csuchico.edu>
2023-10-31 13:26:47 -0700

    added more to move script and changed some objects in scene and added plane

--- GitTest/Assets/scripts/PlayerController.cs
+++ GitTest/Assets/scripts/PlayerController.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Impliment Player Movement
+    public float speed;
+    private Vector3 movePos;
+
+    private void MovePlayer()
+    {
+        // moves the player
+    }
+
+    private void Jump()
+    {
+       //makes player jump
+    }//
     
 }
